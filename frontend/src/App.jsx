7918fdff3e0c89e7c5e321d250bab1031021257e
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-08 07:31:36 +0500

    setup react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./Routes";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/loading/:nextUrl" element={<Loading />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route
+          path="/admin/*"
+          element={
+            user ? (
+              <Layout />
+            ) : (
+              <div className="min-h-screen flex justify-center items-center">
+                <SignIn fallbackRedirectUrl={"/admin"} />
+              </div>
+            )
+          }
+        >
+          <Route index element={<Dashboard />} />
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
+        </Route> */}
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
