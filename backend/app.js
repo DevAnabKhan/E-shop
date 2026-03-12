2b55bb664e1319f5af109a89e757ab921e5ede97
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-12 15:13:19 +0500

    setup cors , multer , routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import { error } from "./middleware/error.js";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/userRoute.js";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use("/", express.static("uploads"));
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
   dotenv.config({
@@ -9,4 +18,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   });
 }
 
+app.use("/api/v2/user", userRouter);
+
+app.use(error);
 export default app;
