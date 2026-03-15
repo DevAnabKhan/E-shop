3625e01d0f69259c7f7f5bc875e8929875adfa08
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-15 18:11:48 +0500

    created activation page and calling activation api

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
 import upload from "../multer.js";
-import { userController } from "../controller/userController.js";
+import {
+  activateUserAccount,
+  userController,
+} from "../controller/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/create-user", upload.single("file"), userController);
+userRouter.post("/activation", activateUserAccount);
 
 export default userRouter;
