e021d366f3608ccd62c217893f098dc3b057ee49
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-15 22:08:17 +0500

    completed user slice/reducer

---
+++ frontend/src/redux/slices/userSlice.js
@@ -0,0 +1,35 @@
+// redux/slices/userSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    isAuthenticated: false,
+    loading: false,
+    user: null,
+    error: null,
+  },
+  reducers: {
+    loadUserRequest: (state) => {
+      state.loading = true;
+    },
+    loadUserSuccess: (state, action) => {
+      state.isAuthenticated = true;
+      state.loading = false;
+      state.user = action.payload;
+    },
+    loadUserFail: (state, action) => {
+      state.isAuthenticated = false;
+      state.loading = false;
+      state.error = action.payload;
+    },
+    clearError: (state) => {
+      state.error = null;
+    },
+  },
+});
+
+export const { loadUserRequest, loadUserSuccess, loadUserFail, clearError } =
+  userSlice.actions;
+
+export const userReducer = userSlice.reducer;
