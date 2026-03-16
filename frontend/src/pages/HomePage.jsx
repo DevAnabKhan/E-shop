bd1df0b1c273c059320505c91eb40a9d79b5fb60
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-16 19:13:07 +0500

    created homepage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Layout/Header";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header activeHeading={1} />
+      <p>home</p>
+    </div>
+  );
+};
+
+export default HomePage;
