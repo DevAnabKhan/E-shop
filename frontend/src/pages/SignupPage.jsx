d097359333c221db9f826debbd1b2d4c65d25970
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-26 10:19:00 +0500

    added navigation

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Signup from "../components/Signup/Signup";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const SignupPage = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");
+    }
+  }, []);
   return (
     <div>
       <Signup />
