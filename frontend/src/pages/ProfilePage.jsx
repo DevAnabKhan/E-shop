61c4db62213789b4aad7881043555c99d05f7901
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-27 07:20:49 +0500

    created profile page

---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Header from "../components/Layout/Header";
+import ProfileSidebar from "../components/Profile/ProfileSidebar";
+import ProfileContent from "../components/Profile/ProfileContent";
+import styles from "../styles/styles";
+
+const ProfilePage = () => {
+  const [active, setActive] = useState(1);
+  return (
+    <div>
+      <Header />
+      <div className={`${styles.section} py-10 flex bg-[#f5f5f5]`}>
+        <div className="w-83.75">
+          <ProfileSidebar active={active} setActive={setActive} />
+        </div>
+        <ProfileContent active={active} setActive={setActive} />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
