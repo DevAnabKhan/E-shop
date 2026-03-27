aa204b15ba3dbda6a006e6ee2d8e86ae4b5600cc
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-27 10:41:44 +0500

    updated navbar

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = ({ active }) => {
             <Link
               to={i.url}
               className={`${
-                active === index + 1 ? "text-[#17dd1f]" : "text-[#fff]"
-              } font-[500] px-6 cursor-pointer`}
+                active === index + 1 ? "text-[#17dd1f]" : "text-white"
+              } font-medium px-6 cursor-pointer`}
             >
               {i.title}
             </Link>
