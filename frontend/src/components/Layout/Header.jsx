68221b2a1b4222eefe9b718bb86072cfaad28cff
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-27 21:25:27 +0500

    added responsive header

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -16,6 +16,7 @@ import { useSelector } from "react-redux";
 import { backend_url } from "../../server.js";
 import Cart from "../Cart/Cart.jsx";
 import Wishlist from "../Wishlist/Wishlist.jsx";
+import { RxCross1 } from "react-icons/rx";
 
 const Header = ({ activeHeading }) => {
   const { isAuthenticated, user, loading } = useSelector((state) => state.user);
@@ -25,6 +26,7 @@ const Header = ({ activeHeading }) => {
   const [dropDown, setDropDown] = useState(false);
   const [openCart, setOpenCart] = useState(false);
   const [openWishlist, setOpenWishlist] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleSearch = (e) => {
     const term = e.target.value;
@@ -194,6 +196,130 @@ const Header = ({ activeHeading }) => {
                 {openWishlist && <Wishlist setOpenWishlist={setOpenWishlist} />}
               </div>
             </div>
+          </div>
+
+          <div
+            className={`${
+              active === true ? "shadow-sm fixed top-0 left-0 z-10" : null
+            }
+      w-full h-15 bg-white z-50 top-0 left-0 shadow-sm 800px:hidden`}
+          >
+            <div className="w-full flex items-center justify-between">
+              <div>
+                <BiMenuAltLeft
+                  size={40}
+                  className="ml-4"
+                  onClick={() => setOpen(true)}
+                />
+              </div>
+              <div>
+                <Link to="/">
+                  <img
+                    src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+                    alt=""
+                    className="mt-3 cursor-pointer"
+                  />
+                </Link>
+              </div>
+              <div>
+                <div className="relative mr-5">
+                  <AiOutlineShoppingCart size={30} />
+                  <span className="absolute -top-1 -right-1 rounded-full bg-[#3bc177] w-4 h-4 text-white text-[10px] flex items-center justify-center">
+                    0
+                  </span>
+                </div>
+              </div>
+            </div>
+            {open && (
+              <div className="fixed w-full bg-[#0000005f] z-20 h-full top-0 left-0">
+                <div className="fixed w-[70%] bg-white h-screen top-0 left-0 z-10">
+                  <div className="w-full justify-between pr-3 flex">
+                    <div>
+                      <div className="relative mr-3.75">
+                        <AiOutlineHeart size={30} className="mt-5 ml-3" />
+                        <span className="absolute -top-1 -right-1 rounded-full bg-[#3bc177] w-4 h-4 text-white text-[10px] flex items-center justify-center">
+                          0
+                        </span>
+                      </div>
+                    </div>
+                    <RxCross1
+                      size={30}
+                      className="ml-4 mt-5"
+                      onClick={() => setOpen(false)}
+                    />
+                  </div>
+                  <div className="my-8 w-[92%] m-auto h-10">
+                    <input
+                      placeholder="Search Product"
+                      type="search"
+                      className="h-10 w-full px-2 border-[#3957db] border-2 rounded-md"
+                      onChange={handleSearch}
+                      value={searchTerm}
+                    />
+                    {searchData && (
+                      <div className="absolute bg-white z-10 shadow w-full left-0 p-3">
+                        {searchData.map((i) => {
+                          const d = i.name;
+
+                          const Product_name = d.replace(/\s+/g, "-");
+                          return (
+                            <Link to={`/product/${Product_name}`}>
+                              <div className="flex items-center">
+                                <img
+                                  src={i.image_Url[0]?.url}
+                                  alt=""
+                                  className="w-12.5 mr-2"
+                                />
+                                <h5>{i.name}</h5>
+                              </div>
+                            </Link>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </div>
+                  <Navbar active={activeHeading} />
+                  <div className={`${styles.button} ml-4 rounded-sm!`}>
+                    <Link to="/shop-create">
+                      <h1 className="text-white flex items-center">
+                        Become Seller <IoIosArrowForward className="ml-1" />
+                      </h1>
+                    </Link>
+                  </div>
+                  <br />
+                  <br />
+                  <br />
+                  <div className="flex w-full justify-center">
+                    {isAuthenticated ? (
+                      <div>
+                        <Link to="/profile">
+                          <img
+                            src={`${backend_url}${user?.avatar?.url}`}
+                            alt=""
+                            className="w-15 h-15 rounded-full border-[3px] border-[#0eae88]"
+                          />
+                        </Link>
+                      </div>
+                    ) : (
+                      <>
+                        <Link
+                          to="/login"
+                          className="text-[18px] pr-2.5 text-[#000000b7]"
+                        >
+                          Login /
+                        </Link>
+                        <Link
+                          to="/sign-up"
+                          className="text-[18px] text-[#000000b7]"
+                        >
+                          Sign up
+                        </Link>
+                      </>
+                    )}
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </>
       )}
