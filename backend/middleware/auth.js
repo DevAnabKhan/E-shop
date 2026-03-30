92aa4963117f648b3fb06f6758a47d44e69532da
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-30 23:11:13 +0500

    created auth middleware for shop

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,6 +2,7 @@ import User from "../model/userModel.js";
 import { catchAsyncErrors } from "./catchAsyncErrors.js";
 import jwt from "jsonwebtoken";
 import ErrorHandler from "../utils/ErrorHandler.js";
+import Shop from "../model/shopModel.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
@@ -13,6 +14,21 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
   req.user = await User.findById(decoded.id);
+
+  next();
+});
+
+export const isShopAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const { seller_token } = req.cookies;
+  console.log(seller_token);
+  if (!seller_token) {
+    return next(new ErrorHandler("Please login to continue", 401));
+  }
+
+  const decoded = jwt.verify(seller_token, process.env.JWT_SECRET_KEY);
+  console.log(decoded);
+  req.shop = await Shop.findById(decoded.id);
+  console.log("Shop from auth middleware:", req.shop);
 
   next();
 });
