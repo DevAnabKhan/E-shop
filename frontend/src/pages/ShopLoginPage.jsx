d34fe3be2aaeed3f9234a80efe78ddae0fbd2363
DevAnabKhan <anabkhan8148@gmail.com>
2026-03-30 23:14:06 +0500

    created shop login page

---
+++ frontend/src/pages/ShopLoginPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ShopLogin from "../components/Shop/ShopLogin";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+
+const ShopLoginPage = () => {
+  const navigate = useNavigate();
+  const { isShopAuthenticated, shop } = useSelector((state) => state.shop);
+  console.log(
+    "ShopLoginPage rendered, isShopAuthenticated:",
+    isShopAuthenticated,
+  );
+  useEffect(() => {
+    if (isShopAuthenticated && shop?._id) {
+      console.log("Shop authenticated, navigating...");
+      navigate(`/shop/${shop._id}`);
+    }
+  }, [isShopAuthenticated, shop, navigate]);
+  return (
+    <div>
+      <ShopLogin />
+    </div>
+  );
+};
+
+export default ShopLoginPage;
