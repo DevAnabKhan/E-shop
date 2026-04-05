aa079e771f9919904c38dd4301e2572bdc81edb0
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-05 08:23:49 +0500

    created dashboard header component

---
+++ frontend/src/components/Shop/Layout/DashboardHeader.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { AiOutlineGift } from "react-icons/ai";
+import { MdOutlineLocalOffer } from "react-icons/md";
+import { FiPackage, FiShoppingBag } from "react-icons/fi";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { backend_url } from "../../../server";
+
+const DashboardHeader = () => {
+  const { shop } = useSelector((state) => state.shop);
+  return (
+    <div className="w-full h-20 bg-white sticky shadow top-0 left-0 z-30 flex items-center justify-between px-4  ">
+      <div>
+        <Link to="/dashboard">
+          <img
+            src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+            alt=""
+          />
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <div className="flex items-center mr-4">
+          <Link to={"/dashboard/coupons"} className="800:block hidden">
+            <AiOutlineGift
+              color="#555"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to={"/dashboard-events"} className="800:block hidden">
+            <MdOutlineLocalOffer
+              color="#555"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to={"/dashboard-products"} className="800:block hidden">
+            <FiShoppingBag
+              color="#555"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to={"/dashboard-orders"} className="800:block hidden">
+            <FiPackage color="#555" size={30} className="mx-5 cursor-pointer" />
+          </Link>
+          <Link to={"/dashboard-messages"} className="800:block hidden">
+            <BiMessageSquareDetail
+              color="#555"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to={`/shop/${shop._id}`}>
+            <img
+              src={`${backend_url}${shop?.avatar?.url}`}
+              className="w-8.75 h-8.75 rounded-full"
+              alt=""
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardHeader;
