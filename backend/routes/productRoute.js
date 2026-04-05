66cbbf5dd968ee07894ef5334098173e7d937110
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-05 08:21:24 +0500

    created product route

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import upload from "../multer.js";
+import {
+  createProduct,
+  deleteShopProduct,
+  getAllProducts,
+} from "../controller/productController.js";
+import { isShopAuthenticated } from "../middleware/auth.js";
+
+const productRoute = express.Router();
+
+productRoute.post("/create-product", upload.array("images"), createProduct);
+productRoute.get("/get-all-products-shop/:id", getAllProducts);
+productRoute.delete(
+  "/delete-shop-product/:id",
+  isShopAuthenticated,
+  deleteShopProduct,
+);
+
+export default productRoute;
