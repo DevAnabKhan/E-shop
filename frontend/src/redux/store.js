c2f1903984c2fdeccef477b717aa9a752f5dc326
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-05 08:28:51 +0500

    updated store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import React from "react";
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slices/userSlice";
 import { shopReducer } from "./slices/shopSlice";
+import { productReducer } from "./slices/productSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     shop: shopReducer,
+    product: productReducer,
   },
 });
 
