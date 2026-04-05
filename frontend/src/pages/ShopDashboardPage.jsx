309499232bfb59640821660686cbed6e5fdf2662
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-05 08:25:46 +0500

    created shop dashboard page

---
+++ frontend/src/pages/ShopDashboardPage.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import DashboardHeader from "../components/Shop/Layout/DashboardHeader";
+import DashboardSidebar from "../components/Shop/Layout/DashboardSidebar";
+
+const ShopDashboardPage = () => {
+  const [active, setActive] = useState(1);
+  return (
+    <div>
+      <DashboardHeader />
+      <div className="w-full flex items-center justify-between">
+        <div className="w-20 800:w-82.5">
+          <DashboardSidebar active={active} setActive={setActive} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopDashboardPage;
