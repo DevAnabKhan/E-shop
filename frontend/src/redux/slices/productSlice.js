1594aaf1682fe01e2db590b26ef546955fa42124
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-05 08:28:14 +0500

    created product slice

---
+++ frontend/src/redux/slices/productSlice.js
@@ -0,0 +1,79 @@
+// redux/slices/userSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    productLoading: true,
+    product: null,
+    error: null,
+    success: false,
+    fetchLoading: false,
+    fetchError: null,
+    fetchSuccess: false,
+    allProducts: [],
+    deleteError: false,
+    deleteLoading: false,
+    message: null,
+  },
+  reducers: {
+    loadProductRequest: (state) => {
+      state.productLoading = true;
+    },
+    loadProductSuccess: (state, action) => {
+      state.productLoading = false;
+      state.product = action.payload;
+      state.success = true;
+    },
+    loadProductFail: (state, action) => {
+      state.productLoading = false;
+      state.error = action.payload;
+      state.success = false;
+    },
+
+    fetchAllProductsRequest: (state) => {
+      state.fetchLoading = true;
+    },
+    fetchAllProductsSuccess: (state, action) => {
+      state.fetchLoading = false;
+      state.allProducts = action.payload;
+    },
+    fetchAllProductsFail: (state, action) => {
+      state.fetchLoading = false;
+      state.fetchError = action.payload;
+    },
+    deleteProductRequest: (state) => {
+      state.deleteLoading = true;
+    },
+    deleteProductSuccess: (state, action) => {
+      state.deleteLoading = false;
+      state.message = action.payload;
+      state.allProducts = state.allProducts.filter(
+        (product) => product._id !== action.payload.id,
+      );
+    },
+    deleteProductFail: (state, action) => {
+      state.deleteLoading = false;
+      state.deleteError = action.payload;
+    },
+    clearError: (state) => {
+      state.error = null;
+    },
+  },
+});
+
+export const {
+  loadProductRequest,
+  loadProductSuccess,
+  loadProductFail,
+  fetchAllProductsFail,
+  fetchAllProductsRequest,
+  fetchAllProductsSuccess,
+  deleteProductFail,
+  deleteProductRequest,
+  deleteProductSuccess,
+
+  clearError,
+} = productSlice.actions;
+
+export const productReducer = productSlice.reducer;
