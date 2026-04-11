4a18ce2a7e3573ca7f877ce91b1a14823c151188
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-11 12:04:29 +0500

    show dynamic events

--- frontend/src/components/Route/Events/Events.jsx
+++ frontend/src/components/Route/Events/Events.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import EventCard from "./EventCard";
 import styles from "../../../styles/styles";
+import { useSelector } from "react-redux";
 
 const Events = () => {
+  const { allUserEvents, isUserEventsLoading } = useSelector(
+    (state) => state.event,
+  );
   return (
-    <div className={`${styles.section}`}>
-      <div className={`${styles.heading}`}>
-        <h1>Popular Events</h1>
-      </div>
-      <div className="w-full grid">
-        <EventCard />
-      </div>
+    <div>
+      {!isUserEventsLoading && (
+        <div className={`${styles.section}`}>
+          <div className={`${styles.heading}`}>
+            <h1>Popular Events</h1>
+          </div>
+
+          <div className="w-full grid">
+            {allUserEvents.length !== 0 && (
+              <EventCard data={allUserEvents && allUserEvents[0]} />
+            )}
+            <h4>{allUserEvents?.length === 0 && "No Events have!"}</h4>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
