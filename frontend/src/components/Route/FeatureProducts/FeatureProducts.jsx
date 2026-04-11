6d6b05754a8b58a7cb322a9235399fb14ec7961a
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-11 12:04:54 +0500

    show dynamic featured shops

--- frontend/src/components/Route/FeatureProducts/FeatureProducts.jsx
+++ frontend/src/components/Route/FeatureProducts/FeatureProducts.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import styles from "../../../styles/styles";
 import { productData } from "../../../static/data";
 import ProductCard from "../ProductCard/ProductCard";
+import { useSelector } from "react-redux";
 
 const FeatureProducts = () => {
+  const { allUserProducts } = useSelector((state) => state.product);
+  console.log(allUserProducts);
   return (
     <div className={`${styles.section}`}>
       <div className={`${styles.heading}`}>
         <h1>Featured Products</h1>
       </div>
-      <div className="grid grid-cols-1 gap-[20px] md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
-        {productData &&
-          productData.map((i, index) => <ProductCard data={i} key={index} />)}
+      <div className="grid grid-cols-1 gap-5 md:gap-6.25 lg:grid-cols-4 lg:gap-6.25 xl:grid-cols-5 xl:gap-7.5 mb-12 border-0">
+        {allUserProducts &&
+          allUserProducts.map((i, index) => (
+            <ProductCard data={i} key={index} />
+          ))}
       </div>
     </div>
   );
