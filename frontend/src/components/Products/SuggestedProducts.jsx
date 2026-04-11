f24b42cc01019b1339f5d4f641b154e7797f3f0b
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-11 12:02:58 +0500

    show dynamic product data

--- frontend/src/components/Products/SuggestedProducts.jsx
+++ frontend/src/components/Products/SuggestedProducts.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import styles from "../../styles/styles";
 import ProductCard from "../Route/ProductCard/ProductCard";
-import { productData } from "../../static/data";
+import { useSelector } from "react-redux";
 
 const SuggestedProducts = ({ data }) => {
-  const [products, setProducts] = useState([]);
+  const { allUserProducts } = useSelector((state) => state.product);
+  const [product, setProduct] = useState();
 
   useEffect(() => {
     const d =
-      productData && productData.filter((i) => i.category === data.category);
+      allUserProducts &&
+      allUserProducts.filter((i) => i.category === data.category);
 
-    setProducts(d);
+    setProduct(d);
   }, []);
   return (
     <div>
@@ -21,9 +23,9 @@ const SuggestedProducts = ({ data }) => {
           >
             Related Products
           </h2>
-          <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-            {products &&
-              products.map((i, index) => <ProductCard data={i} key={index} />)}
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-6.25 lg:grid-cols-4 lg:gap-6.25 xl:grid-cols-5 xl:gap-7.5 mb-12">
+            {product &&
+              product.map((i, index) => <ProductCard data={i} key={index} />)}
           </div>
         </div>
       )}
