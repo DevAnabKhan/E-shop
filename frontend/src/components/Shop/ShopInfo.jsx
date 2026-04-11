b633ec8c4ff99dc18be7cbf73ddba246cfd661e7
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-11 12:07:48 +0500

    added dynamic shop info

--- frontend/src/components/Shop/ShopInfo.jsx
+++ frontend/src/components/Shop/ShopInfo.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { backend_url, server } from "../../server";
 import styles from "../../styles/styles";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
+import Loader from "../Layout/Loader";
 
 const ShopInfo = ({ isOwner }) => {
-  const { shop } = useSelector((state) => state.shop);
+  const { allProducts } = useSelector((state) => state.product);
   const navigate = useNavigate();
+  const { id } = useParams();
+  const [active, setActive] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState({});
+  console.log(allProducts);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (id) {
+        try {
+          setIsLoading(true);
+          const res = await axios.get(`${server}/shop/get-shop-info/${id}`);
+
+          if (res.data.success) {
+            setData(res.data.shop);
+            setIsLoading(false);
+            // ✅ IMPORTANT FIX
+          }
+        } catch (error) {
+          setIsLoading(false);
+          console.log(error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [id]);
   const logoutHandler = async () => {
     try {
       const res = await axios.get(`${server}/shop/logout-shop`, {
@@ -25,52 +52,66 @@ const ShopInfo = ({ isOwner }) => {
     }
   };
   return (
-    <div>
-      <div className="w-full py-5">
-        <div className="w-full flex items-center justify-center">
-          <img
-            src={`${backend_url}${shop?.avatar?.url}`}
-            className="w-37.5 h-37.5 object-cover rounded-full"
-            alt=""
-          />
-        </div>
-        <h3 className="text-center py-2 text-[20px]"> {shop.name}</h3>
-        <p className="text-center p-2.5 text-[16px] flex text-[#000000a6]">
-          {shop.description}
-        </p>
-      </div>
-      <div className="p-3">
-        <h5 className="font-semibold">Address</h5>
-        <h4 className="text-[#000000a6]">{shop.address}</h4>
-      </div>
-      <div className="p-3">
-        <h5 className="font-semibold">Total Products</h5>
-        <h4 className="text-[#000000a6]">10</h4>
-      </div>
-      <div className="p-3">
-        <h5 className="font-semibold">Shop Ratings</h5>
-        <h4 className="text-[#000000b0]">4/5</h4>
-      </div>
-      <div className="p-3">
-        <h5 className="font-semibold">Joined On</h5>
-        <h4 className="text-[#000000b0]">{shop.createdAt?.slice(0, 10)}</h4>
-      </div>
-      {isOwner && (
-        <div className="py-3 px-4">
-          <Link to="/settings">
-            <div className={`${styles.button} w-full! h-10.5! rounded-[5px]!`}>
-              <span className="text-white">Edit Shop</span>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <div className="w-full py-5">
+            <div className="w-full flex items-center justify-center">
+              <img
+                src={`${backend_url}${data.avatar?.url}`}
+                className="w-37.5 h-37.5 object-cover rounded-full"
+                alt=""
+              />
             </div>
-          </Link>
-          <div
-            className={`${styles.button} w-full! h-10.5! rounded-[5px]!`}
-            onClick={logoutHandler}
-          >
-            <span className="text-white">Log Out</span>
+            <h3 className="text-center py-2 text-[20px]"> {data.name}</h3>
+            <p className="text-center p-2.5 text-[16px] flex text-[#000000a6]">
+              {data.description}
+            </p>
           </div>
+          <div className="p-3">
+            <h5 className="font-semibold">Address</h5>
+            <h4 className="text-[#000000a6]">{data.address}</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-semibold">Phone Number</h5>
+            <h4 className="text-[#000000a6]">{data.phoneNumber}</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-semibold">Total Products</h5>
+            <h4 className="text-[#000000a6]">
+              {allProducts && allProducts.length}
+            </h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-semibold">Shop Ratings</h5>
+            <h4 className="text-[#000000b0]">4/5</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-semibold">Joined On</h5>
+            <h4 className="text-[#000000b0]">{data.createdAt?.slice(0, 10)}</h4>
+          </div>
+          {isOwner && (
+            <div className="py-3 px-4">
+              <Link to="/settings">
+                <div
+                  className={`${styles.button} w-full! h-10.5! rounded-[5px]!`}
+                >
+                  <span className="text-white">Edit Shop</span>
+                </div>
+              </Link>
+              <div
+                className={`${styles.button} w-full! h-10.5! rounded-[5px]!`}
+                onClick={logoutHandler}
+              >
+                <span className="text-white">Log Out</span>
+              </div>
+            </div>
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 };
 
