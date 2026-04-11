06faf8578caac59c5b5f33d18abed8ba60b5aea6
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-11 12:47:58 +0500

    some changes

--- frontend/src/pages/ProductDetailsPage.jsx
+++ frontend/src/pages/ProductDetailsPage.jsx
@@ -2,18 +2,24 @@ import React, { useEffect, useState } from "react";
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
 import { useParams } from "react-router-dom";
-import { productData } from "../static/data";
 import ProductsDetails from "../components/Products/ProductsDetails";
 import SuggestedProducts from "../components/Products/SuggestedProducts";
+import { useSelector } from "react-redux";
 
 const ProductDetailsPage = () => {
-  const { name } = useParams();
+  const { id } = useParams();
   const [data, setData] = useState(null);
-  const productName = name.replace(/-/g, " ");
+  const { allUserProducts } = useSelector((state) => state.product);
+
   useEffect(() => {
-    const data = productData.find((item) => item.name === productName);
-    setData(data);
-  }, []);
+    const foundData = allUserProducts.find((item) => item._id === id);
+    setData(foundData);
+  }, [allUserProducts, id]);
+  // useEffect(() => {
+  //   const data = allUserProducts.find((item) => item._id === id);
+  //   setData(data);
+  //   console.log("data in product detail page", data);
+  // }, [data, allUserProducts]);
   return (
     <div>
       <Header />
