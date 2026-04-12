3289cf565925f03c930936ae64e4a4a940d7307b
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-12 12:41:51 +0500

    updated protected route

--- frontend/src/protectedRoutes/ProtectedRoute.jsx
+++ frontend/src/protectedRoutes/ProtectedRoute.jsx
@@ -4,9 +4,9 @@ import { Navigate } from "react-router-dom";
 import Loader from "../components/Layout/Loader";
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, isLoading } = useSelector((state) => state.user);
+  const { isAuthenticated, loading } = useSelector((state) => state.user);
 
-  if (isLoading === true) {
+  if (loading === true) {
     return <Loader />;
   } else {
     return isAuthenticated ? children : <Navigate to="/login" replace />;
