23493a2f4063874770689e6264383fe7ddd5992f
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-12 12:44:47 +0500

    updated store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,12 +3,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slices/userSlice";
 import { shopReducer } from "./slices/shopSlice";
 import { productReducer } from "./slices/productSlice";
+import { eventReducer } from "./slices/eventSlice";
+import { cartReducer } from "./slices/cartSlice";
+import { wishlistReducer } from "./slices/wishlistSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     shop: shopReducer,
     product: productReducer,
+    event: eventReducer,
+    cart: cartReducer,
+    wishlist: wishlistReducer,
   },
 });
 
