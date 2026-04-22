616953d18de51a819cf7514a3fbbb190958242bc
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-22 22:00:49 +0500

    created routes

---
+++ backend/routes/messageRoute.js
@@ -0,0 +1,17 @@
+import express from "express";
+import upload from "../multer.js";
+import {
+  createNewMessage,
+  getAllMessages,
+} from "../controller/messageController.js";
+
+const messageRoute = express.Router();
+
+messageRoute.post(
+  "/create-new-message",
+  upload.array("images"),
+  createNewMessage,
+);
+messageRoute.get("/get-all-messages/:id", getAllMessages);
+
+export default messageRoute;
