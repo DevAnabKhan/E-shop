5b2af9550965c6eebe76f069412b183f0d7156ad
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-22 22:00:42 +0500

    created routes

--- backend/routes/couponCodeRoute.js
+++ backend/routes/couponCodeRoute.js
@@ -4,12 +4,14 @@ import {
   createCoupon,
   deleteCouponCode,
   getAllCoupons,
+  getCouponValue,
 } from "../controller/couponCodeController.js";
 
 const couponCodeRoute = express.Router();
 
 couponCodeRoute.post("/create-coupon", isShopAuthenticated, createCoupon);
 couponCodeRoute.get("/get-all-coupon/:id", isShopAuthenticated, getAllCoupons);
+couponCodeRoute.get("/get-coupon-value/:name", getCouponValue);
 couponCodeRoute.delete(
   "/delete-coupon/:id",
   isShopAuthenticated,
