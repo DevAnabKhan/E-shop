d795b7bcb6ac83d8975d5cd97a7b2ed4126e88e3
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-26 23:55:19 +0500

    created shop order refund component

---
+++ frontend/src/components/Orders/ShopAllOrderRefunds.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Loader from "../Layout/Loader";
+import { DataGrid } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { getAllOrdersOfShop } from "../../redux/actions/order";
+
+const ShopAllOrderRefunds = () => {
+  const { loading, shopOrders } = useSelector((state) => state.order);
+  const { shop } = useSelector((state) => state.shop);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (shop?._id) {
+      console.log("calling");
+      dispatch(getAllOrdersOfShop(shop._id));
+    }
+  }, [dispatch, shop?._id]);
+
+  const refundOrders =
+    shopOrders &&
+    shopOrders.filter(
+      (item) =>
+        item.status === "Processing refund" || item.status === "Refund Success",
+    );
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.row.status === "Delivered" ? "greenColor" : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/order/${params.id}`}>
+              <Button>
+                <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  refundOrders &&
+    refundOrders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.cart.length,
+        total: "US$ " + item.totalPrice,
+        status: item.status, // ← correct
+      });
+    });
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="w-full  mx-8 pt-1 mt-10 bg-white">
+          <DataGrid
+            rows={row}
+            columns={columns}
+            pageSize={10}
+            disableRowSelectionOnClick
+            autoHeight
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ShopAllOrderRefunds;
