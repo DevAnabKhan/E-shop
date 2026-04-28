b29aa38bc622dda3a4622bc406c21561baa94f50
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-28 11:15:44 +0500

    updated dashboard

--- frontend/src/pages/ShopDashboardPage.jsx
+++ frontend/src/pages/ShopDashboardPage.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import DashboardHeader from "../components/Shop/Layout/DashboardHeader";
 import DashboardSidebar from "../components/Shop/Layout/DashboardSidebar";
+import DashboardHero from "../components/Shop/DashboardHero";
 
 const ShopDashboardPage = () => {
   const [active, setActive] = useState(1);
   return (
     <div>
       <DashboardHeader />
-      <div className="w-full flex items-center justify-between">
+      <div className="w-full flex items-start justify-between">
         <div className="w-20 800:w-82.5">
           <DashboardSidebar active={active} setActive={setActive} />
         </div>
+        <DashboardHero />
       </div>
     </div>
   );
