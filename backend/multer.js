8888fc8de57c34880bfe77224f835bc12e67a9d8
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-28 11:10:21 +0500

    updated multer

--- backend/multer.js
+++ backend/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, res, cb) {
-    cb(null, "uploads/");
+    cb(null, "../uploads/");
   },
 
   filename: function (req, file, cb) {
