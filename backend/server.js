aba5a464828d77f12f00f32bf5a69e06b38c7586
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-28 11:10:54 +0500

    update code

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import app from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/Database.js";
 
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({
+    path: "./config/.env",
+  });
+}
 await connectDB();
 
 process.on("uncaughtException", (err) => {
@@ -9,17 +14,11 @@ process.on("uncaughtException", (err) => {
   console.log("shutting down server for handling uncaught Exception ");
 });
 
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  dotenv.config({
-    path: "./config/.env",
-  });
-}
-
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
-process.on("unhandleRejection", (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(`shutting down server for $${err.message}`);
   console.log("shutting down server for unhandle promise Rejection ");
 
