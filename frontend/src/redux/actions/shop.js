ff782d66fd1f8c93afafe2f86c121896151c81be
DevAnabKhan <anabkhan8148@gmail.com>
2026-04-28 11:18:27 +0500

    updated shop actions

--- frontend/src/redux/actions/shop.js
+++ frontend/src/redux/actions/shop.js
@@ -3,6 +3,9 @@ import axios from "axios";
 import { server } from "../../server";
 
 import {
+  getAllShopFail,
+  getAllShopRequest,
+  getAllShopSuccess,
   loadShopFail,
   loadShopRequest,
   loadShopSuccess,
@@ -18,6 +21,19 @@ export const loadShop = () => async (dispatch) => {
   } catch (error) {
     dispatch(
       loadShopFail(error.response?.data?.message || "Something went wrong"),
+    );
+  }
+};
+export const getAllShopsForAdmin = () => async (dispatch) => {
+  try {
+    dispatch(getAllShopRequest());
+
+    const { data } = await axios.get(`${server}/shop/get-all-admin-shops`);
+
+    dispatch(getAllShopSuccess(data.shops));
+  } catch (error) {
+    dispatch(
+      getAllShopFail(error.response?.data?.message || "Something went wrong"),
     );
   }
 };
